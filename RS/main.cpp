f273d02b58865ad936ab0194d3b29ec75e2daf10
redouane.dziri <redouane.dziri@polytechnique.edu>
2017-12-18 12:16:48 +0100

    0:)

--- RS/main.cpp
+++ RS/main.cpp
@@ -1,12 +1,21 @@
 #include "multiclass_rs.h"
 
-#include <time.h>
+
+#include <iostream>
+
+using namespace std;
 
 int main() {
 
+    int numberOfTrees = 3;
+    int row_count = 11;
     string train = "apprentissage.txt";
     string test = "test_full.txt";
-    multiclass_rs * classifier1 = new multiclass_rs(13, 10);
-    int * predictions_random_forest = classifier1->run_random_forest(train, test);
+    multiclass_rs * classifier1 = new multiclass_rs(numberOfTrees, 10);
+    double * predictions_random_forest = classifier1->run_random_forest(train, test);
+    for (int j = 0; j < row_count; j++) {
+        cout << predictions_random_forest[j] << endl;
+    }
+
 
 }
